e9b99cbe5308de5d1ca5bd2a2b138d4ed7fc809b
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2020-04-22 00:15:17 +0900

    fix logging

--- python/src/main.py
+++ python/src/main.py
@@ -1,4 +1,4 @@
-import logging
+from logging import getLogger, StreamHandler, DEBUG, Formatter, FileHandler
 import feedparser
 import datetime
 import db.database as db
@@ -18,31 +18,31 @@ ENDPOINT = "http://rais.skr.u-ryukyu.ac.jp/dc/?feed=rss2"
 # エラーが起きた際にこの webhook にエラー内容を送信する。
 MYADDRESS = ""
 
-ENVIRONMENTFILE = "./../sample.env"
+ENVIRONMENTFILE = "./../.env"
 
 ##########
 LIVE_SERVER = 0
 TEST_SERVER = 1
 DRY_RUN = 2
+
+
 ##########
 
 
 def main():
-    status = init()
-
-    # 現在時刻をログに記載
-    logging.info(datetime.datetime.now())
+    status, logger = init()
 
     # ENDPOINTにアクセスしrssを取得
+    logger.info("Access to endpoint")
     feeds = feedparser.parse(ENDPOINT)
 
-    conn = db.mysql_connecter()
-    cur = conn.cursor()
+    database = db.DB(logger=logger)
+    cur = database.conn.cursor()
 
     cur.execute("select * from feeds")
     # DB上のデータを取得
     rows = cur.fetchall()
-    conn.commit()
+    database.conn.commit()
 
     feed_count = len(feeds['entries'])  # 処理するfeedの量
     for i in range(feed_count):
@@ -50,23 +50,42 @@ def main():
         title_in_feed = feeds['entries'][i]['title']
         url_in_feed = feeds['entries'][i]['link']
         # DB上に存在しなければ
-        if not db.if_feed_exist(rows, url_in_feed):
+        if not database.if_feed_exist(rows, url_in_feed):
             if status != DRY_RUN:  # dry-runの場合は実行しない
                 cur.execute("INSERT INTO feeds VALUES (%s, %s)", (title_in_feed, url_in_feed))
-                conn.commit()
-            mm = mattermost.Mattermost(status)
+                database.conn.commit()
+            mm = mattermost.Mattermost(status, logger=logger)
             mm.send(title_in_feed, url_in_feed)
 
     # セッションを切断
     cur.close()
-    conn.close()
+    database.conn.close()
+    logger.info("Successful completion")
 
 
-# logging の設定と引数の処理を行います。 戻り値にスクリプトの引数を返します。
+def init_loger(log_folder, modname=__name__):
+    logger = getLogger(modname)
+    logger.setLevel(DEBUG)
+
+    sh = StreamHandler()
+    sh.setLevel(DEBUG)
+    formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    fh = FileHandler(log_folder)  # fh = file handler
+    fh.setLevel(DEBUG)
+    fh_formatter = Formatter('%(asctime)s - %(filename)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s')
+    fh.setFormatter(fh_formatter)
+    logger.addHandler(fh)
+    return logger
+
+
+# logging の設定と引数の処理を行います。 戻り値にスクリプト実行時の引数と logger を返します。.
 def init():
+    logger = init_loger(log_folder="./../sedona.log", modname="debug")
     # loggingの設定
-    logging.basicConfig(filename='./../sedona.log', level=logging.DEBUG)
-    logging.info("**** sedona start ****")
+    logger.info("sedona start")
 
     # 引数の処理
     status = DRY_RUN  # とりあえず安全な値を初期値にする
@@ -74,32 +93,32 @@ def init():
         dotenv.load_dotenv(ENVIRONMENTFILE)
         args = sys.argv
         if args[1] == "live-server":
-            logging.info("****** THIS IS LIVE-SERVER ******")
+            logger.info("THIS IS LIVE-SERVER")
             status = LIVE_SERVER
         elif args[1] == "test-server":
-            logging.info("****** THIS IS TEST-SERVER ******")
+            logger.info("THIS IS TEST-SERVER")
             status = TEST_SERVER
         elif args[1] == "dry-run":
-            logging.info("****** THIS IS DRY-RUN ******")
+            logger.info("THIS IS DRY-RUN")
         else:
-            logging.error("Arguments Error")
-            logging.error("*****  (example) python main.py dry-run  *****")
+            logger.error("Arguments Error")
+            logger.error("example python main.py dry-run")
             error("Arguments Error")
             exit(1)
 
     except KeyError as e:
         # .env関係のエラー
-        logging.error("Environment variable error")
-        logging.error(e)
+        logger.error("Environment variable error")
+        logger.error(e)
         error(e)
         exit(1)
     except IndexError as e:
         # 引数関係のエラー
-        logging.error(e)
+        logger.error(e)
         error(e)
         exit(1)
 
-    return status
+    return status, logger
 
 
 def error(message):
